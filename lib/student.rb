44ae80a005ed6405363a1b0ee902ec29eb48093d
Zachary Sain <zachsain@gmail.com>
2022-04-05 14:20:01 -0500

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
 
+    attr_accessor :knowledge
+    
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end 
+
+    def learn(string)
+        @knowledge << string
+    end 
 end
